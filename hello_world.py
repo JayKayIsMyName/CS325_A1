da028507f2f99fe89c47d15a699072702b788137
JayKayIsMyName <beaujksmith1210@gmail.com>
2024-06-05 15:36:01 -0500

    I'm gonna make a moderately high pay!

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import time
+
+print("Addition Calculator")
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+print("Calculating sum...")
+
+time.sleep(3)
+
+print("Hello world!")
